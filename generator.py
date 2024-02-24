061b535d386a3c771cb44fac2cdfc01be0071065
Laércio Filho <santannaf.laercio@gmail.com>
2024-02-24 08:35:03 -0300

    Initial commit

---
+++ generator.py
@@ -0,0 +1,40 @@
+import json
+import random
+
+# Open and load the JSON configuration file
+with open("config/config.json", "r") as f:
+    config_json = json.load(f)
+
+# Extract specific requirements and number of passwords from the configuration
+specific_requirements = config_json["specific_requirements"]
+number_of_passwords = config_json["number_of_passwords"]
+
+
+def generate_password():
+    """
+    Function to generate a password based on the configuration file.
+
+    The function first sets the default characters for the password.
+    Then, it checks if digits and symbols are required in the password.
+    If they are, it adds them to the characters.
+    Finally, it generates a password of the specified length from these characters.
+
+    Returns:
+        str: The generated password
+    """
+    # Start with the default characters
+    characters = config_json["default_characters"]
+    # If digits are required, add them to the characters
+    if specific_requirements["digits"]:
+        characters += config_json["allowed_digits"]
+    # If symbols are required, add them to the characters
+    if specific_requirements["symbols"]:
+        characters += config_json["allowed_symbols"]
+
+    # Generate a password of the specified length from the characters
+    password = "".join(random.choice(characters) for _ in range(config_json["password_length"]))
+    return password
+
+
+# Print the generated password
+print(generate_password())
